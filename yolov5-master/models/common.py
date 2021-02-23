40ed59dae2bd661a600d88743b52aa6d032ba67f
q1135718080 <1135718080@qq.com>
2021-02-23 14:42:26 +0800

    update56

--- yolov5-master/models/common.py
+++ yolov5-master/models/common.py
@@ -277,3 +277,26 @@ class SELayer(nn.Module):
         y = self.avg_pool(x).view(b, c)
         y = self.fc(y).view(b, c, 1, 1)
         return x * y.expand_as(x)
+
+
+# CBAM
+class ChannelAttention(nn.Module):
+    def __init__(self, channel, reduction=16):
+        super(ChannelAttention, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+
+        self.fc = nn.Sequential(
+            nn.Linear(channel, channel // reduction, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(channel // reduction, channel, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y1 = self.avg_pool(x).view(b, c)
+        y2 = self.avg_pool(x).view(b, c)
+        y = y1 + y2
+        y = self.fc(y).view(b, c, 1, 1)
+        return x * y.expand_as(x)
